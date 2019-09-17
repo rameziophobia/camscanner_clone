e285165a83cc8854f08e0ce36f3d82c534cfd232
RamezIOPhobia <rameznoshy@gmail.com>
2019-09-17 16:08:24 +0200

    add rotating images

--- camscanner.py
+++ camscanner.py
@@ -1,22 +1,70 @@
 import cv2
 import numpy as np
 import pytesseract
+import imutils
 from PIL import Image
 
-img = cv2.imread('red.png')
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+img = cv2.imread('2_30d.png')
+
+# convert the image to grayscale and flip the foreground
+# and background to ensure foreground is now "white" and
+# the background is "black"
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_,thresh=cv2.threshold(gray,60,255,1)
-blur = cv2.medianBlur(thresh,1)
+gray = cv2.bitwise_not(gray)
+
+# threshold the image, setting all foreground pixels to
+# 255 and all background pixels to 0
+thresh = cv2.threshold(gray, 0, 255,
+                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+# grab the (x, y) coordinates of all pixel values that
+# are greater than zero, then use these coordinates to
+# compute a rotated bounding box that contains all
+# coordinates
+coords = np.column_stack(np.where(thresh > 0))
+angle = cv2.minAreaRect(coords)[-1]
+
+# the `cv2.minAreaRect` function returns values in the
+# range [-90, 0); as the rectangle rotates clockwise the
+# returned angle trends to 0 -- in this special case we
+# need to add 90 degrees to the angle
+if angle < -45:
+    angle = -(90 + angle)
+
+# otherwise, just take the inverse of the angle to make
+# it positive
+else:
+    angle = -angle
+
+# rotate the image to deskew it
+(h, w) = img.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+# rotated = imutils.rotate_bound(img, angle=angle)
+# rotated = imutils.rotate_bound(img, angle=-angle)
+
+# draw the correction angle on the image so we can validate it
+cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
+            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+# show the output image
+print("[INFO] angle: {:.3f}".format(angle))
+cv2.imshow("Input", img)
+cv2.imshow("Rotated", rotated)
+cv2.waitKey(0)
 
 
+gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
+_, thresh = cv2.threshold(gray, 60, 255, 1)
+blur = cv2.medianBlur(thresh, 1)
 
-print(pytesseract.image_to_string(blur))
+print(pytesseract.image_to_string(gray))
 
 cv2.imshow('ImageWindow1', gray)
 cv2.imshow('ImageWindow2', thresh)
 cv2.imshow('ImageWindow3', blur)
 
 cv2.waitKey(0)
-
-
