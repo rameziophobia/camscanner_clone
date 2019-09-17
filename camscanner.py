6a1ba3354b114bad556e86579a117c727486dc85
RamezIOPhobia <rameznoshy@gmail.com>
2019-09-17 18:04:02 +0200

    add arabic detection

--- camscanner.py
+++ camscanner.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
 import pytesseract
-# from pyimagesearch.transform import four_point_transform
 import imutils
-from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread('2_30d_border.png')
+img = cv2.imread('arabic.png')
+
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
-# image = cv2.imread(args["image"])
 ratio = img.shape[0] / 500.0
 orig = img.copy()
 img = imutils.resize(img, height=500)
@@ -47,34 +45,38 @@ for c in cnts:
         screenCnt = approx
         break
 
+# if no contour was found it will try to detect blacks
+# as text ( if only a part of the document was pictured)
 if screenCnt is None:
-    print("haha")
+    # convert the image to grayscale and flip the foreground
+    # and background to ensure foreground is now "white" and
+    # the background is "black"
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
 
-# show the contour (outline) of the piece of paper
-print("STEP 2: Find contours of paper")
-cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 2)
-cv2.imshow("Outline", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # threshold the image, setting all foreground pixels to
+    # 255 and all background pixels to 0
+    thresh = cv2.threshold(gray, 0, 255,
+                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-# convert the image to grayscale and flip the foreground
-# and background to ensure foreground is now "white" and
-# the background is "black"
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bitwise_not(gray)
+    # grab the (x, y) coordinates of all pixel values that
+    # are greater than zero, then use these coordinates to
+    # compute a rotated bounding box that contains all
+    # coordinates
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
 
-# threshold the image, setting all foreground pixels to
-# 255 and all background pixels to 0
-thresh = cv2.threshold(gray, 0, 255,
-                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# else
+else:
+    # show the contour (outline) of the piece of paper
+    print("STEP 2: Find contours of paper")
+    cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 2)
+    cv2.imshow("Outline", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# grab the (x, y) coordinates of all pixel values that
-# are greater than zero, then use these coordinates to
-# compute a rotated bounding box that contains all
-# coordinates
-coords = np.column_stack(np.where(thresh > 0))
-angle = cv2.minAreaRect(coords)[-1]
-angle = cv2.minAreaRect(screenCnt)[-1]
+    angle = cv2.minAreaRect(screenCnt)[-1]
+
 
 # the `cv2.minAreaRect` function returns values in the
 # range [-90, 0); as the rectangle rotates clockwise the
@@ -90,16 +92,12 @@ else:
 angle = -angle
 
 # rotate the image to deskew it
-(h, w) = img.shape[:2]
+(h, w) = orig.shape[:2]
 center = (w // 2, h // 2)
 M = cv2.getRotationMatrix2D(center, angle, 1.0)
-rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+rotated = cv2.warpAffine(orig, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 # rotated = imutils.rotate_bound(img, angle=angle)
 # rotated = imutils.rotate_bound(img, angle=-angle)
-
-# draw the correction angle on the image so we can validate it
-# cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
-#             (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 # show the output image
 print("[INFO] angle: {:.3f}".format(angle))
@@ -112,7 +110,7 @@ gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 60, 255, 1)
 blur = cv2.medianBlur(thresh, 1)
 
-print(pytesseract.image_to_string(gray))
+print(pytesseract.image_to_string(gray, lang='ara'))
 
 cv2.imshow('ImageWindow1', gray)
 cv2.imshow('ImageWindow2', thresh)
