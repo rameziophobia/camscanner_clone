794e566593bacc8a50156c02de4e7b270a779897
RamezIOPhobia <rameznoshy@gmail.com>
2019-09-17 17:37:00 +0200

    add edge detection

--- camscanner.py
+++ camscanner.py
@@ -1,12 +1,61 @@
 import cv2
 import numpy as np
 import pytesseract
+# from pyimagesearch.transform import four_point_transform
 import imutils
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread('2_30d.png')
+img = cv2.imread('2_30d_border.png')
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+# image = cv2.imread(args["image"])
+ratio = img.shape[0] / 500.0
+orig = img.copy()
+img = imutils.resize(img, height=500)
+
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", img)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+screenCnt = None
+# loop over the contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+if screenCnt is None:
+    print("haha")
+
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # convert the image to grayscale and flip the foreground
 # and background to ensure foreground is now "white" and
@@ -25,6 +74,7 @@ thresh = cv2.threshold(gray, 0, 255,
 # coordinates
 coords = np.column_stack(np.where(thresh > 0))
 angle = cv2.minAreaRect(coords)[-1]
+angle = cv2.minAreaRect(screenCnt)[-1]
 
 # the `cv2.minAreaRect` function returns values in the
 # range [-90, 0); as the rectangle rotates clockwise the
@@ -37,6 +87,7 @@ if angle < -45:
 # it positive
 else:
     angle = -angle
+angle = -angle
 
 # rotate the image to deskew it
 (h, w) = img.shape[:2]
@@ -47,8 +98,8 @@ rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.B
 # rotated = imutils.rotate_bound(img, angle=-angle)
 
 # draw the correction angle on the image so we can validate it
-cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
-            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+# cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
+#             (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 # show the output image
 print("[INFO] angle: {:.3f}".format(angle))
